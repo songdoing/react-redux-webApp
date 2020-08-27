2718b27ee630d7fd5c30ec5dcfdbe88e221e9a04
wonjinsong <msmk22239@gmail.com>
2020-08-27 00:17:54 -0400

    Updates

---
+++ precache-manifest.a479cd25092613b9442bea7d601d34f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e80bc78bde8ed57bdc07464ecb59e31",
+    "url": "/react-redux-webApp/index.html"
+  },
+  {
+    "revision": "05a381e7ff5200ee2bd3",
+    "url": "/react-redux-webApp/static/css/main.95bd51c0.chunk.css"
+  },
+  {
+    "revision": "959c1d8f29a2465a1ca0",
+    "url": "/react-redux-webApp/static/js/2.90595a0c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-webApp/static/js/2.90595a0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05a381e7ff5200ee2bd3",
+    "url": "/react-redux-webApp/static/js/main.c2d99909.chunk.js"
+  },
+  {
+    "revision": "c32ce011cfbba0cd3b33",
+    "url": "/react-redux-webApp/static/js/runtime-main.97be5035.js"
+  }
+]);
